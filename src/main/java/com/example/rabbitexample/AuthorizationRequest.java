6ddf6d08a8562716393f365fcc8fbca25ce42a5c
Steve Womack <swomack@pivotal.io>
2019-08-16 13:55:16 -0500

    Adds mvn dir

--- src/main/java/com/example/rabbitexample/AuthorizationRequest.java
+++ src/main/java/com/example/rabbitexample/AuthorizationRequest.java
@@ -1,4 +1,4 @@
-package io.pivotal.domain;
+package com.example.rabbitexample;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
--- src/test/java/io/pivotal/AuthorizationRequestWorkerApplicationTests.java
+++ src/test/java/io/pivotal/AuthorizationRequestWorkerApplicationTests.java
@@ -1,12 +1,9 @@
 package io.pivotal;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = AuthorizationRequestWorkerApplication.class)
+@SpringBootTest
 public class AuthorizationRequestWorkerApplicationTests {
 
 	@Test
